1d476d6f5fc339565992fc4abca9dfd03e3030a1
whenzei <e0032088@u.nus.edu>
2021-07-13 20:56:56 +0800

    update test config

--- config/testConfig.js
+++ config/testConfig.js
@@ -8,15 +8,16 @@ var Config = async function(accounts) {
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
-        "0x69e1CB5cFcA8A311586e3406ed0301C06fb839a2",
-        "0xF014343BDFFbED8660A9d8721deC985126f189F3",
-        "0x0E79EDbD6A727CfeE09A2b1d0A59F7752d5bf7C9",
-        "0x9bC1169Ca09555bf2721A5C9eC6D69c8073bfeB4",
-        "0xa23eAEf02F9E0338EEcDa8Fdd0A73aDD781b2A86",
-        "0x6b85cc8f612d5457d49775439335f83e12b8cfde",
-        "0xcbd22ff1ded1423fbc24a7af2148745878800024",
-        "0xc257274276a4e539741ca11b590b9447b26a8051",
-        "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
+        "0xC999De768BCfBfafF80ed71100DC94CFF0eaf847",
+        "0xd079eFF796c1F60b3A7929620cE7853194B3Cbf8",
+        "0x2495078e3717b47e1047cd6D6B6d230459baeB5c",
+        "0x059bf972Bf3e64bE544d3ea2E6Fe2A8377a3F4b4",
+        "0x29547fac3a848f009C2D57Ac1Fbe782DC149780C",
+        "0x55a9585b9fffA4BEdf438badc9C1A6407aa55C47",
+        "0x362c86b9e9B85194C257FaFd03DB134019e582ae",
+        "0x2E24Fc859186D86b95b6E170bA4b68f7A20800eb",
+        "0xaAAC94e5CeDF5Ac8754Bd7de3D68F6b520f0ca13",
+        "0x00473290F4448261a5DeBec4904d1402b7eCC206"
     ];
 
 
